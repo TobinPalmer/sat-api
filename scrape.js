be6805070e53037047dfb79bd0cb93f60a8c18e3
TobinPalmer <tobinkpalmer@gmail.com>
2024-08-06 09:55:47 -0700

    changed to a more dynamic approach with MutationObservers

--- scrape.js
+++ scrape.js
@@ -1,24 +1,59 @@
-(() => {
-    const arr = [...document.querySelectorAll(".view-question-button")]
-    const obj = {}
+(async () => {
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
 
-    let i = 0;
-    setInterval(() => {
-        arr[i].click()
-        setTimeout(() => {
-            const question = document.querySelector(".question")
-            const answerChoices = document.querySelector(".answer-choices")
-            const answer = document.querySelector(".answer-content")
-            obj[arr[i].textContent] = {
-                question: question.innerHTML,
-                answerChoices: answerChoices.innerHTML,
-                answer: answer.textContent,
-                difficulty: document.querySelector(".tqdifficulty").getAttribute('aria-label').toLowerCase(),
-                skill: document.querySelector("div.col-sm:nth-child(4) > div:nth-child(2)").textContent,
-                domain: document.querySelector('div.col-sm:nth-child(3) > div:nth-child(2)').textContent,
-            }
-        }, 1000)
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+
+  const arr = [...document.querySelectorAll(".view-question-button")]
+  const obj = {}
+
+  async function next(idx) {
+    arr[idx].click()
+    // const question = document.querySelector(".question")
+    // const answerChoices = document.querySelector(".answer-choices")
+    // const answer = document.querySelector(".answer-content")
+    const question = await waitForElm(".question")
+    const answerChoices = await waitForElm(".answer-choices")
+    const answer = await waitForElm(".answer-content")
+
+    const difficulty = (await waitForElm(".tqdifficulty")).getAttribute('aria-label').toLowerCase()
+    const skill = (await waitForElm("div.col-sm:nth-child(4) > div:nth-child(2)")).textContent
+    const domain = (await waitForElm('div.col-sm:nth-child(3) > div:nth-child(2)')).textContent
+
+    obj[arr[idx].textContent] = {
+      question: question.innerHTML,
+      answerChoices: answerChoices.innerHTML,
+      answer: answer.textContent,
+      difficulty,
+      skill,
+      domain,
+    }
+    waitForElm(".cb-btn-close").then(e => {
+      const base = 500
+      setTimeout(() => {
+        e.click()
+      }, base)
+
+      setTimeout(() => {
         console.log(obj)
-        i++
-    }, 3000)
+        next(idx + 1)
+      }, base + 300)
+    })
+  }
+  next(0)
 })()
