c46b8190d9bbdb354889d85d8a4596730476cab2
TobinPalmer <tobinkpalmer@gmail.com>
2024-08-06 07:18:46 -0700

    pdf reading mostly working

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import PyPDF2
+
+# creating a pdf reader object
+reader = PyPDF2.PdfReader('math.pdf')
+
+# print the number of pages in pdf file
+print(len(reader.pages))
+
+# print the text of the first page
+print(reader.pages[1].extract_text())
 
 app = Flask(__name__)
 
